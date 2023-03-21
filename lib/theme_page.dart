fa24d5561604a821305877b534fc4151080e8399
Wachiranan Phuangpanya <wachiranan_p@cmu.ac.th>
2023-03-21 16:11:17 +0700

    edit to show theme that owned

--- lib/theme_page.dart
+++ lib/theme_page.dart
@@ -20,18 +20,52 @@ class _ThemeListState extends State<ThemeList> {
           body: GridView.builder(
               itemCount: alarmService.theme.length,
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3),
+                  crossAxisCount: 3, childAspectRatio: 0.7),
               itemBuilder: ((context, index) {
                 return Container(
                   margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
                   child: GestureDetector(
-                    child: Image.asset(alarmService.theme[index]![0]),
-                    onTap: () {},
+                    child: Column(
+                      children: [
+                        Image.asset(alarmService.theme[index]![0]),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: !alarmService.theme[index]![3]
+                                ? const [
+                                    Icon(
+                                      Icons.attach_money,
+                                      color: Colors.yellow,
+                                    ),
+                                    Text("100",
+                                        style: TextStyle(color: Colors.yellow))
+                                  ]
+                                : [
+                                    const Text("Owned",
+                                        style: TextStyle(color: Colors.white))
+                                  ])
+                      ],
+                    ),
+                    onTap: () {
+                      //ตอนกดแล้วซื้อทำตรงนี้
+                    },
                   ),
                 );
               })),
         );
       } else {
+        BoxDecoration? myBox() {
+          if (alarmService.isSelected) {
+            return BoxDecoration(
+                border: Border.all(
+                    width: 2,
+                    color: alarmService
+                        .color //                   <--- border width
+                    ));
+          } else {
+            return null;
+          }
+        }
+
         return Scaffold(
           backgroundColor: alarmService.subColor,
           appBar: AppBar(
@@ -39,18 +73,31 @@ class _ThemeListState extends State<ThemeList> {
                 child: Text("Themes", style: TextStyle(color: Colors.white))),
           ),
           body: GridView.builder(
-              itemCount: alarmService.theme.length,
+              itemCount: alarmService.theme.entries
+                  .where((entry) => entry.value[3])
+                  .length,
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 3),
               itemBuilder: ((context, index) {
+                final filteredList = alarmService.theme.entries
+                    .where((entry) => entry.value[3])
+                    .toList();
+                final currentItem = filteredList[index];
                 return Container(
                   margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
                   child: GestureDetector(
-                    child: Image.asset(alarmService.theme[index]![0]),
+                    child: alarmService.chosen == index + 1
+                        ? Container(
+                            decoration: myBox(),
+                            child: Image.asset(currentItem.value[0]),
+                          )
+                        : Image.asset(currentItem.value[0]),
                     onTap: () {
                       setState(() {
-                        alarmService.setColor(alarmService.theme[index]![1]);
-                        alarmService.setSubColor(alarmService.theme[index]![2]);
+                        alarmService.isSelected = true;
+                        alarmService.chosen = index + 1;
+                        alarmService.setColor(currentItem.value[1]);
+                        alarmService.setSubColor(currentItem.value[2]);
                       });
                     },
                   ),
